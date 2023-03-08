8a5b91d49a9b072e7b59b5c1ffe6a9bcba01adf5
Mirella Leoni <109217533+miiswan@users.noreply.github.com>
2023-03-08 12:33:27 -0300

    Add files via upload

---
+++ codificação-frontend/senai/js/dom-events/script.js
@@ -0,0 +1,21 @@
+// DOM (Document Objet Model)
+// A DOM é o HTML convertido para um objeto JavaScript
+// Estrutura de dados do tipo árvore, criada pelo browser (navegador)
+
+// Para que serve?
+// JavaScript usa a DOM para se conectar ao HTML
+// Manipular o HTML com o JavaScript
+// Você só programa na web por causa da DOM
+
+// Manipulando conteúdos
+// - textContent // Possibilita alterar ou adicionar um conteúdo de texto no documento HTML.
+// - innerText // Possibilita alterar ou adicionar um texto interno no do
+// - innerHTML // Possibilita alterar, adicionar e também inserir elementos do HTML
+// - value // Possibilita inserir, alterar, e pegar valores de um input
+
+
+// Manipulando elementos através de seus atributos
+// setAttribute
+// getAttribute
+// removeAttribute
+
